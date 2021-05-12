631fde5a5685958a0a0e5ab1c6c44b1423a0f56a
viviane <v.buschwallace@yahoo.com>
2021-05-12 17:02:54 +0200

    if conditions done for wins

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,32 +12,97 @@ const cellDivs = document.querySelectorAll(".game-cell");
 let xTurn = true;
 
 const checkWinner = () => {
-  const topLeft = document.querySelector(".cell1");
-  const topMiddle = document.querySelector(".cell2");
-  const topRight = document.querySelector(".cell3");
-  const middleLeft = document.querySelector(".cell4");
-  const middleMiddle = document.querySelector(".cell5");
-  const middleRight = document.querySelector(".cell6");
-  const bottomLeft = document.querySelector(".cell7");
-  const bottomMiddle = document.querySelector(".cell8");
-  const bottomRight = document.querySelector(".cell9");
+  const topLeft = document.querySelector(".cell1").classList[2];
+  const topMiddle = document.querySelector(".cell2").classList[2];
+  const topRight = document.querySelector(".cell3").classList[2];
+  const middleLeft = document.querySelector(".cell4").classList[2];
+  const middleMiddle = document.querySelector(".cell5").classList[2];
+  const middleRight = document.querySelector(".cell6").classList[2];
+  const bottomLeft = document.querySelector(".cell7").classList[2];
+  const bottomMiddle = document.querySelector(".cell8").classList[2];
+  const bottomRight = document.querySelector(".cell9").classList[2];
+  let overlay = document.querySelector(".overlay");
 
-  if (topLeft && topLeft == topMiddle && topLeft == topRight) {
-    console.log(topLeft);
+  if (
+    // horizontal top row wins
+    topLeft &&
+    topLeft == topMiddle &&
+    topLeft == topRight
+  ) {
+    overlay.style.display = "flex";
+    statusDiv.innerHTML = `${topLeft} is the winner!`;
+  } else if (
+    // horizontal middle row wins
+    middleLeft &&
+    middleLeft == middleMiddle &&
+    middleLeft == middleRight
+  ) {
+    overlay.style.display = "flex";
+    statusDiv.innerHTML = `${middleLeft} is the winner!`;
+  } else if (
+    // horizontal bottom row wins
+    bottomLeft &&
+    bottomLeft == bottomMiddle &&
+    bottomLeft == bottomRight
+  ) {
+    overlay.style.display = "flex";
+    statusDiv.innerHTML = `${bottomLeft} is the winner!`;
+  } else if (
+    // vertical left column wins
+    topLeft &&
+    topLeft == middleLeft &&
+    topLeft == bottomLeft
+  ) {
+    overlay.style.display = "flex";
+    statusDiv.innerHTML = `${topLeft} is the winner!`;
+  } else if (
+    // vertical middle column wins
+    topMiddle &&
+    topMiddle == middleMiddle &&
+    topMiddle == bottomMiddle
+  ) {
+    overlay.style.display = "flex";
+    statusDiv.innerHTML = `${topMiddle} is the winner!`;
+  } else if (
+    // vertical right column wins
+    topRight &&
+    topRight == middleRight &&
+    topRight == bottomRight
+  ) {
+    overlay.style.display = "flex";
+    statusDiv.innerHTML = `${topRight} is the winner!`;
+  } else if (
+    // diagonal topleft to bottomright wins
+    topLeft &&
+    topLeft == middleMiddle &&
+    topLeft == bottomRight
+  ) {
+    overlay.style.display = "flex";
+    statusDiv.innerHTML = `${topLeft} is the winner!`;
+  } else if (
+    // diagonal topright to bottomleft wins
+    topRight &&
+    topRight == middleMiddle &&
+    topRight == bottomLeft
+  ) {
+    overlay.style.display = "flex";
+    statusDiv.innerHTML = `${topRight} is the winner!`;
   }
 };
-
 const clickEvent = (e) => {
   if (xTurn) {
     e.target.innerHTML = "X";
     e.target.classList.add("x");
     xTurn = false;
+    statusDiv.innerHTML = "It's O's turn.";
+    checkWinner();
   } else {
     e.target.innerHTML = "O";
     e.target.classList.add("o");
     xTurn = true;
+    statusDiv.innerHTML = "It's X's turn.";
+    checkWinner();
   }
-  checkWinner;
 };
 
 for (const cellDiv of cellDivs) {
