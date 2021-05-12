171d0acd68e1a8ee4b5fc46dde60f0d71985ae6f
viviane <v.buschwallace@yahoo.com>
2021-05-12 14:11:09 +0200

    insert x on click

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,45 +9,53 @@ const cellDivs = document.querySelectorAll(".game-cell");
 // check if 3 align (8cases) or if no winner
 // clicked cell cannot take another symbol
 
-// ============================================ Guide =======================
-
-// game constants
-const xSymbol = "×";
-const oSymbol = "○";
-
-// game variables
-let gameIsLive = true;
-let xIsNext = true;
-
-// functions
-const letterToSymbol = (letter) => (letter === "x" ? xSymbol : oSymbol);
-
-const handleWin = (letter) => {
-  // change the gameIsLive status to false and add the winning message to the statusDiv
+const clickEvent = (e) => {
+  e.target.innerHTML = "X";
 };
-
-const checkGameStatus = () => {
-  // grab the cells second class from the cellDivs array and assign them to the variables (topLeft,topMiddle,topRight, middleLeft, middleMiddle,...)
-
-  // check winner
-  if (topLeft && topLeft === topMiddle && topLeft === topRight) {
-  }
-};
-
-// event Handlers
-const handleReset = () => {};
-
-const handleCellClick = (e) => {
-  const classList = e.target.classList;
-
-  // check if the game is still live and the targeted div has no 'X' or 'O' class
-
-  // check whose turn it is and then check the game status
-};
-
-// event listeners
-resetDiv.addEventListener("click", handleReset);
 
 for (const cellDiv of cellDivs) {
-  cellDiv.addEventListener("click", handleCellClick);
+  cellDiv.addEventListener("click", clickEvent);
 }
+
+// ============================================ Guide =======================
+
+// // game constants
+// const xSymbol = "×";
+// const oSymbol = "○";
+
+// // game variables
+// let gameIsLive = true;
+// let xIsNext = true;
+
+// // functions
+// const letterToSymbol = (letter) => (letter === "x" ? xSymbol : oSymbol);
+
+// const handleWin = (letter) => {
+//   // change the gameIsLive status to false and add the winning message to the statusDiv
+// };
+
+// const checkGameStatus = () => {
+//   // grab the cells second class from the cellDivs array and assign them to the variables (topLeft,topMiddle,topRight, middleLeft, middleMiddle,...)
+
+//   // check winner
+//   if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+//   }
+// };
+
+// // event Handlers
+// const handleReset = () => {};
+
+// const handleCellClick = (e) => {
+//   const classList = e.target.classList;
+
+//   // check if the game is still live and the targeted div has no 'X' or 'O' class
+
+//   // check whose turn it is and then check the game status
+// };
+
+// // event listeners
+// resetDiv.addEventListener("click", handleReset);
+
+// for (const cellDiv of cellDivs) {
+//   cellDiv.addEventListener("click", handleCellClick);
+// }
