56312953daa436e759fb3d58a2f7962c738c8a81
viviane <v.buschwallace@yahoo.com>
2021-08-10 14:11:06 +0200

    just added some comments and it's a tie sentence for some results

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,14 @@
-// HTML Elements
+// HTML elements
 const statusDiv = document.querySelector(".status");
 const resetDiv = document.querySelector(".reset");
 const cellDivs = document.querySelectorAll(".game-cell");
 
-// the overlay keeps the user from clicking more gridtiles
+// the overlay keeps the user from clicking more gridtiles after a winner is found
 let overlay = document.querySelector(".overlay");
 
 // check if there are 3 same symbols in a row
 const checkWinner = () => {
+  // cell's values
   const topLeft = document.querySelector(".cell1").innerHTML;
   const topMiddle = document.querySelector(".cell2").innerHTML;
   const topRight = document.querySelector(".cell3").innerHTML;
@@ -90,20 +91,19 @@ const checkWinner = () => {
     overlay.style.display = "flex";
     statusDiv.innerHTML = `${topRight} is the winner!`;
     statusDiv.style.backgroundColor = "green";
+  } else if (
+    topLeft &&
+    topMiddle &&
+    topRight &&
+    middleLeft &&
+    middleMiddle &&
+    middleRight &&
+    bottomLeft &&
+    bottomMiddle &&
+    bottomRight
+  ) {
+    statusDiv.innerHTML = "It's a tie!";
   }
-};
-
-const reset = () => {
-  overlay.style.display = "none";
-  console.log("hi");
-  cellDivs.forEach((e) => {
-    e.innerHTML = "";
-    e.classList.remove("x");
-    e.classList.remove("o");
-    xTurn = true;
-    statusDiv.innerHTML = "It's X's turn.";
-    statusDiv.style.backgroundColor = "transparent";
-  });
 };
 
 // What happens on clicking a grid tile
@@ -127,51 +127,42 @@ const clickEvent = (e) => {
   }
 };
 
+// clicking reset
+const reset = () => {
+  overlay.style.display = "none";
+  console.log("hi");
+  cellDivs.forEach((e) => {
+    e.innerHTML = "";
+    e.classList.remove("x");
+    e.classList.remove("o");
+    xTurn = true;
+    statusDiv.innerHTML = "It's X's turn.";
+    statusDiv.style.backgroundColor = "rgb(0, 136, 84)";
+  });
+};
+
+// event listeners
 resetDiv.addEventListener("click", reset);
 
 for (const cellDiv of cellDivs) {
   cellDiv.addEventListener("click", clickEvent);
 }
 
-// ============================================ Guide =======================
+let arr = [0, 1, 2, 3];
+console.log(arr.slice(0, 2)); // output: [0, 1]
+console.log(arr); // output: [0, 1, 2, 3]; slice does not affect the original array
+console.log(arr.slice(0)); // output: [0, 1, 2, 3]
+console.log(arr.splice(0, 2)); // output: [0, 1]
+console.log(arr); // output: [2, 3]; splice DOES affect the original array
+arr = [0, 1, 2, 3];
+console.log(arr.splice(0)); // output: [0, 1, 2, 3]
+console.log("Chetna's");
+console.log([1, 2, 3].slice(2));
+console.log([1, 2, 3, 4].slice(0, 2));
+console.log([6, 7, 8].splice(0, 1, 5));
+console.log([1, 2, 3, 4].shift());
+console.log([1, 2, 3].unshift(4));
+console.log([1, 2, 3].pop());
 
-// // game constants
-// const xSymbol = "×";
-// const oSymbol = "○";
-
-// // game variables
-// let gameIsLive = true;
-// let xIsNext = true;
-
-// // functions
-// const letterToSymbol = (letter) => (letter === "x" ? xSymbol : oSymbol);
-
-// const handleWin = (letter) => {
-//   // change the gameIsLive status to false and add the winning message to the statusDiv
-// };
-
-// const checkGameStatus = () => {
-//   // grab the cells second class from the cellDivs array and assign them to the variables (topLeft,topMiddle,topRight, middleLeft, middleMiddle,...)
-
-//   // check winner
-//   if (topLeft && topLeft === topMiddle && topLeft === topRight) {
-//   }
-// };
-
-// // event Handlers
-// const handleReset = () => {};
-
-// const handleCellClick = (e) => {
-//   const classList = e.target.classList;
-
-//   // check if the game is still live and the targeted div has no 'X' or 'O' class
-
-//   // check whose turn it is and then check the game status
-// };
-
-// // event listeners
-// resetDiv.addEventListener("click", handleReset);
-
-// for (const cellDiv of cellDivs) {
-//   cellDiv.addEventListener("click", handleCellClick);
-// }
+console.log([1, 2].concat(3));
+console.log([1, 2].concat([3]));
